b028c6c5ba636064c829e6e09c45cd94b4c736db
Michal Muskala <michal@muskala.eu>
2015-10-30 10:34:13 +0100

    Simple adding

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,17 @@
 public class Calculator {
     public double eval(String expr) {
-        return 2;
+        String[] parts = expr.split("\\s");
+
+        double lhs = Double.parseDouble(parts[0]);
+        double rhs = Double.parseDouble(parts[2]);
+
+        char op = parts[1].charAt(0);
+
+        switch (op) {
+            case '+':
+                return lhs + rhs;
+            default:
+                throw new RuntimeException("Invalid operation " + op);
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,7 @@ public class CalculatorTest {
     @Test
     public void it_should_add_integers() {
         Calculator calc = new Calculator();
-        assertEquals(2, calc.eval("1+1"), 0);
+        assertEquals(2.0, calc.eval("1 + 1"), 0);
+        assertEquals(4.0, calc.eval("1 + 3"), 0);
     }
 }
